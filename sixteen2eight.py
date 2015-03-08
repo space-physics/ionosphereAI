e2fb8bfd2cfb4590206f13b6f555081b158663bd
scivision <scivision@users.noreply.github.com>
2015-03-07 19:39:13 -0500

    cleanup

--- sixteen2eight.py
+++ sixteen2eight.py
@@ -1,20 +1,17 @@
 from __future__ import division
-
-#from pdb import set_trace
 """
 inputs:
 -------
 I: 2-D Numpy array of grayscale image data
 Clim: length 2 of tuple or numpy 1-D array specifying lowest and highest expected values in grayscale image
+Michael Hirsch
 """
 
 def sixteen2eight(I,Clim):
 
     Vmin = Clim[0]; Vmax = Clim[1]
 
-    Q = I.astype('float64').copy()
-     #clip high end, boost low end
-
-    Q = (Q.clip(Vmin, Vmax) - Vmin) / (Vmax - Vmin) #stretch to [0,1]
+     #clip high end, boost low end (copy not needed)
+    Q = (I.clip(Vmin, Vmax) - Vmin) / (Vmax - Vmin) #stretch to [0,1] (float)
     Q *= 255. # stretch to [0,255] as a float
     return Q.round().astype('uint8') # convert to uint8
