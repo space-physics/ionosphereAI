84908e583a42ac126afb4b2ecfdabc89052a8089
scivision <scivision@users.noreply.github.com>
2015-07-02 18:06:22 -0400

    functionalize

--- detectaurora.py
+++ detectaurora.py
@@ -31,31 +31,35 @@ from tempfile import gettempdir
 from warnings import warn
 #
 try:
+    from .cvops import dooptflow,dothres,dodespeck,domorph,doblob
     from .getpassivefm import getfmradarframe
     from .histutils.walktree import walktree
     from .histutils.rawDMCreader import getDMCparam,getDMCframe,getserialnum
 except:
+    from cvops import dooptflow,dothres,dodespeck,domorph,doblob
     from getpassivefm import getfmradarframe
     from histutils.walktree import walktree
     from histutils.rawDMCreader import getDMCparam,getDMCframe,getserialnum
 
 #plot disable
-showraw=False #often not useful due to no autoscale
-showrawscaled=False      #True  #why not just showfinal
-showhist=False
-showflowvec = False
-showflowhsv = False
-showthres = True      #True
-showofmag = False
-showmeanmedian = False
-showmorph = False      #True
-showfinal = True
-plotdet = False
+pshow = ('thres','final')
+#'raw' #often not useful due to no autoscale
+#'rawscaled'      #True  #why not just showfinal
+#'hist' ogram
+# 'flowvec'
+#'flowhsv'
+#'thres'
+#'ofmag'
+#'meanmedian'
+#'morph'
+#'final'
+#'det'
+         
 savedet = False
-cmplvl = 4 #tradeoff b/w speed and filesize for TIFF
+complvl = 4 #tradeoff b/w speed and filesize for TIFF
 
 #only import matplotlib if needed to save time
-if savedet or plotdet or showhist or showofmag or showmeanmedian:
+if savedet or np.in1d(('det','hist','ofmag','meanmedian'),pshow).any():
     from matplotlib.pylab import draw, pause, figure, hist
     from matplotlib.colors import LogNorm
     #from matplotlib.cm import get_cmap
@@ -63,7 +67,7 @@ if savedet or plotdet or showhist or showofmag or showmeanmedian:
 try:
     import h5py
 except ImportError as e:
-    print('** h5py not working. Wont be able to save detections to disk')
+    warn('h5py not working. Wont be able to save detections to disk')
     print(str(e))
     savedet=False
 
@@ -110,24 +114,25 @@ def procaurora(f,s,camparam,up,savevideo,framebyframe,verbose=False):
         if framegray is None: break
 #%% compute optical flow or Background/Foreground
         if lastflow is not None: #very fast way to check mode
-            flow,ofmaggmm,ofmed,pl = dooptflow(framegray,frameref,lastflow,uv,ifrm, ap,cp,pl)
+            flow,ofmaggmm,ofmed,pl = dooptflow(framegray,frameref,lastflow,uv,
+                                               ifrm, ap,cp,pl,pshow)
             lastflow = flow.copy() #I didn't check if the .copy() is strictly necessary
         else: #background/foreground
             ofmaggmm = gmm.apply(framegray)
 #%% threshold
-        thres = dothres(ofmaggmm, ofmed, ap,cp,svh)
+        thres = dothres(ofmaggmm, ofmed, ap,cp,svh,pshow)
 #%% despeckle
-        despeck = dodespeck(thres,cp['medfiltsize'],svh)
+        despeck = dodespeck(thres,cp['medfiltsize'],svh,pshow)
 #%% morphological ops
-        morphed = domorph(despeck,kern,svh)
+        morphed = domorph(despeck,kern,svh,pshow)
 #%% blob detection
-        final = doblob(morphed,blobdetect,framegray,ifrm,svh,pl,savevideo)
+        final = doblob(morphed,blobdetect,framegray,ifrm,svh,pl,pshow) #lint:ok
 #%% plotting in loop
         """
         http://docs.opencv.org/modules/highgui/doc/user_interface.html
         """
 
-        if plotdet or showhist or showofmag or showmeanmedian:
+        if np.in1d(('det','hist','ofmag','meanmedian'),pshow).any():
             draw(); pause(0.001)
 
         if not ifrm % 50:
@@ -209,7 +214,7 @@ def svsetup(savevideo,ap, cp, up):
     if savevideo:
         print('dumping video output to '+tdir)
     svh = {'video':None, 'wiener':None,'thres':None,'despeck':None,
-           'erode':None,'close':None,'detect':None}
+           'erode':None,'close':None,'detect':None,'save':savevideo,'complvl':complvl}
     if savevideo == 'tif':
         #complvl = 6 #0 is uncompressed
         try:
@@ -225,11 +230,11 @@ def svsetup(savevideo,ap, cp, up):
         else:
             svh['wiener'] = None
 
-        svh['video']  = TiffWriter(join(tdir,'video.tif')) if showrawscaled else None
-        svh['thres']  = TiffWriter(join(tdir,'thres.tif')) if showthres else None
-        svh['despeck']= TiffWriter(join(tdir,'despk.tif')) if showthres else None
-        svh['erode']  = TiffWriter(join(tdir,'erode.tif')) if showmorph else None
-        svh['close']  = TiffWriter(join(tdir,'close.tif')) if showmorph else None
+        svh['video']  = TiffWriter(join(tdir,'video.tif')) if 'rawscaled' in pshow else None
+        svh['thres']  = TiffWriter(join(tdir,'thres.tif')) if 'thres' in pshow else None
+        svh['despeck']= TiffWriter(join(tdir,'despk.tif')) if 'thres' in pshow else None
+        svh['erode']  = TiffWriter(join(tdir,'erode.tif')) if 'morph' in pshow else None
+        svh['close']  = TiffWriter(join(tdir,'close.tif')) if 'morph' in pshow else None
         # next line makes big file
         svh['detect'] = None #TiffWriter(join(tdir,'detect.tif')) if showfinal else None
 
@@ -255,12 +260,12 @@ def svsetup(savevideo,ap, cp, up):
         else:
             svh['wiener'] = None
 
-        svh['video']  = cv2.VideoWriter(join(tdir,'video.avi'), cc4,wfps, (ypix,xpix),False) if showrawscaled else None
-        svh['thres']  = cv2.VideoWriter(join(tdir,'thres.avi'), cc4,wfps, (ypix,xpix),False) if showthres else None
-        svh['despeck']= cv2.VideoWriter(join(tdir,'despk.avi'), cc4,wfps, (ypix,xpix),False) if showthres else None
-        svh['erode']  = cv2.VideoWriter(join(tdir,'erode.avi'), cc4,wfps, (ypix,xpix),False) if showmorph else None
-        svh['close']  = cv2.VideoWriter(join(tdir,'close.avi'), cc4,wfps, (ypix,xpix),False) if showmorph else None
-        svh['detect'] = cv2.VideoWriter(join(tdir,'detct.avi'), cc4,wfps, (ypix,xpix),True) if showfinal else None
+        svh['video']  = cv2.VideoWriter(join(tdir,'video.avi'), cc4,wfps, (ypix,xpix),False) if 'rawscaled' in pshow else None
+        svh['thres']  = cv2.VideoWriter(join(tdir,'thres.avi'), cc4,wfps, (ypix,xpix),False) if 'thres' in pshow else None
+        svh['despeck']= cv2.VideoWriter(join(tdir,'despk.avi'), cc4,wfps, (ypix,xpix),False) if 'thres' in pshow else None
+        svh['erode']  = cv2.VideoWriter(join(tdir,'erode.avi'), cc4,wfps, (ypix,xpix),False) if 'morph' in pshow else None
+        svh['close']  = cv2.VideoWriter(join(tdir,'close.avi'), cc4,wfps, (ypix,xpix),False) if 'morph' in pshow else None
+        svh['detect'] = cv2.VideoWriter(join(tdir,'detct.avi'), cc4,wfps, (ypix,xpix),True)  if 'final' in pshow else None
 
         for k,v in svh.items():
             if v is not None and not v.isOpened():
@@ -404,15 +409,15 @@ def getraw(dfid,ifrm,finf,svh,ap,cp,savevideo,verbose):
     if dowiener:
         framegray = wiener(framegray,cp['wienernhood'])
 
-    if showraw:
+    if 'raw' in pshow:
         # cv2.imshow just divides by 256, NOT autoscaled!
         # http://docs.opencv.org/modules/highgui/doc/user_interface.html
         cv2.imshow('video', framegray)
 #%% plotting
-    if showrawscaled:
+    if 'rawscaled' in pshow:
         cv2.imshow('raw video, scaled to 8-bit', framegray)
     # image histograms (to help verify proper scaling to uint8)
-    if showhist:
+    if 'hist' in pshow:
         figure(321).clf()
         ax=figure(321).gca()
         hist(frame16.flatten(), bins=128, fc='w',ec='k', log=True)
@@ -426,206 +431,17 @@ def getraw(dfid,ifrm,finf,svh,ap,cp,savevideo,verbose):
 
     if svh['video'] is not None:
         if savevideo == 'tif':
-            svh['video'].save(framegray,compress=cmplvl)
+            svh['video'].save(framegray,compress=complvl)
         elif savevideo == 'vid':
             svh['video'].write(framegray)
 
     return framegray,frameref,ap
 
-def dooptflow(framegray,frameref,lastflow,uv,ifrm,ap,cp,pl):
-
-    if ap['ofmethod'] == 'hs':
-        """
-        http://docs.opencv.org/modules/legacy/doc/motion_analysis.html
-        """
-        cvref = cv.fromarray(frameref)
-        cvgray = cv.fromarray(framegray)
-        #result is placed in u,v
-        # matlab vision.OpticalFlow Horn-Shunck has default maxiter=10, terminate=eps, smoothness=1
-        # in TrackingOF7.m I used maxiter=8, terminate=0.1, smaothness=0.1
-        """
-        ***************************
-        Note that smoothness parameter for cv.CalcOpticalFlowHS needs to be SMALLER than matlab
-        to get similar result. Useless when smoothness was 1 in python, but it's 1 in Matlab!
-        *****************************
-        """
-        cv.CalcOpticalFlowHS(cvref, cvgray, False, uv[0], uv[1],
-                             cp['hssmooth'],
-                             (cv.CV_TERMCRIT_ITER | cv.CV_TERMCRIT_EPS, 8, 0.1))
-
-        # reshape to numpy float32, xpix x ypix x 2
-        flow = np.dstack((np.asarray(uv[0]), np.asarray(uv[1])))
-
-    elif ap['ofmethod'] == 'farneback':
-        """
-        http://docs.opencv.org/trunk/modules/video/doc/motion_analysis_and_object_tracking.html
-        """
-        flow = cv2.calcOpticalFlowFarneback(frameref, framegray,
-                                            flow=lastflow, #need flow= for opencv2/3 compatibility
-                                           pyr_scale=0.5,
-                                           levels=1,
-                                           winsize=3,
-                                           iterations=5,
-                                           poly_n = 3,
-                                           poly_sigma=1.5,
-                                           flags=1)
-    else: #using non-of method
-        return None,None,None,None
-#%% zero out edges of image (which have very high flow, unnaturally)
-    '''
-    maybe this can be done more elegantly, maybe via pad or take?
-    http://stackoverflow.com/questions/13525266/multiple-slice-in-list-indexing-for-numpy-array
-    '''
-    te = cp['trimedgeof']
-    flow[:te,...] = 0.; flow[-te:,...] = 0.
-    flow[:,:te,:] = 0.; flow[:,-te:,:] = 0.
-
-    flow /= 255. #make like matlab, which has normalized data input (opencv requires uint8)
-#%% compute median and magnitude
-    ofmag = np.hypot(flow[...,0], flow[...,1])
-    ofmed = np.median(ofmag)
-    if showofmag:
-        pl['median'][ifrm] = ofmed
-        pl['mean'][ifrm] = ofmag.mean()
-        pl['pmed'][0].set_ydata(pl['median'])
-        pl['pmean'][0].set_ydata(pl['mean'])
-
-    if showflowvec:
-        cv2.imshow('flow vectors', draw_flow(framegray,flow) )
-    if showflowhsv:
-        cv2.imshow('flowHSV', draw_hsv(flow) )
-    if showofmag:
-        #cv2.imshow('flowMag', ofmag) #was only grayscale, I wanted color
-        pl['iofm'].set_data(ofmag)
-
-    return flow,ofmag, ofmed, pl
-
-def dothres(ofmaggmm,medianflow,ap,cp,svh):
-    """
-    flow threshold, considering median
-    """
-    if medianflow is not None: #OptFlow based
-        if ap['thresmode'] == 'median':
-            if medianflow>1e-6:  #median is scalar
-                lowthres = cp['ofthresmin'] * medianflow #median is scalar!
-                hithres =  cp['ofthresmax'] * medianflow #median is scalar!
-            else: #median ~ 0
-                lowthres = 0
-                hithres = np.inf
-
-        elif ap['thresmode'] == 'runningmean':
-            exit('*** ' + ap['thresmode'] + ' not yet implemented')
-        else:
-            exit('*** ' + ap['thresmode'] + ' not yet implemented')
-    else:
-        hithres = 255; lowthres=0 #TODO take from spreadsheed as gmmlowthres gmmhighthres
-    """
-    This is the oppostite of np.clip
-    1) make boolean of  min < flow < max
-    2) convert to uint8
-    3) (0,255) since that's what cv2.imshow wants
-    """
-    # the logical_and, *, and & are almost exactly the same speed. The & felt the most Pythonic.
-    #thres = np.logical_and(ofmaggmm < hithres, ofmaggmm > lowthres).astype(np.uint8) * 255
-    thres = ((ofmaggmm<hithres) & (ofmaggmm>lowthres)).astype('uint8') * 255
-    # has to be 0,255 because that's what opencv functions (imshow and computation) want
-
-
-    if svh['thres'] is not None:
-        if savevideo == 'tif':
-            svh['thres'].save(thres,compress=cmplvl)
-        elif savevideo == 'vid':
-            svh['thres'].write(thres)
-
-    if showthres:
-        cv2.imshow('thresholded', thres)
-    """ threshold image by lowThres < abs(OptFlow) < highThres
-    the low threshold helps elimate a lot of "false" OptFlow from camera
-    noise
-    the high threshold helps eliminate star "twinkling," which appears to
-    make very large Optical Flow magnitude
-
-    we multiply boolean by 255 because cv2.imshow expects only values on [0,255] and does not autoscale
-    """
-    return thres
-
-def dodespeck(thres,medfiltsize,svh):
-
-    despeck = cv2.medianBlur(thres,ksize=medfiltsize)
-
-    if svh['despeck'] is not None:
-        if savevideo == 'tif':
-            svh['despeck'].save(despeck,compress=cmplvl)
-        elif savevideo == 'vid':
-            svh['despeck'].write(despeck)
-
-    if showthres:
-        cv2.imshow('despeck', despeck)
-
-    return despeck
-
-def domorph(despeck,kern,svh):
-    """
-    http://docs.opencv.org/master/doc/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
-    """
-   # opened = cv2.morphologyEx(despeck, cv2.MORPH_OPEN, openkernel)
-    eroded = cv2.erode(despeck,kern['erode'])
-    closed = cv2.morphologyEx(eroded, cv2.MORPH_CLOSE, kern['close'])
-
-    if svh['erode'] is not None:
-        if savevideo == 'tif':
-            svh['erode'].save(eroded,compress=cmplvl)
-        elif savevideo == 'vid':
-            svh['erod'].write(eroded)
-
-    if svh['close'] is not None:
-        if savevideo == 'tif':
-            svh['close'].save(closed,compress=cmplvl)
-        elif savevideo == 'vid':
-            svh['close'].write(closed)
-
-    if showmorph:
-        #cv2.imshow('opened', opened)
-        cv2.imshow('morphed',closed)
-
-    return closed
-
-def doblob(morphed,blobdetect,framegray,ifrm,svh,pl,savevideo):
-    """
-    http://docs.opencv.org/master/modules/features2d/doc/drawing_function_of_keypoints_and_matches.html
-    http://docs.opencv.org/trunk/modules/features2d/doc/drawing_function_of_keypoints_and_matches.html
-    """
-    keypoints = blobdetect.detect(morphed)
-    nkey = len(keypoints)
-    final = framegray.copy() # is the .copy necessary?
-
-    final = cv2.drawKeypoints(framegray, keypoints, outImage=final,
-                              flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    cv2.putText(final, text=str(nkey), org=(10,510),
-                fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=5,
-                color=(0,255,0), thickness=2)
-
-    if showfinal:
-        cv2.imshow('final',final)
-
-    if svh['detect'] is not None:
-        if savevideo == 'tif':
-            svh['detect'].save(final,compress=cmplvl)
-        elif savevideo =='vid':
-            svh['detect'].write(final)
-
-    if plotdet or savedet:
-        pl['detect'][ifrm] = nkey
-        pl['pdet'][0].set_ydata(pl['detect'])
-
-    return pl
-
 def setupfigs(finf,fn):
 
 
     hiom = None
-    if showofmag:
+    if 'ofmag' in pshow:
         figure(30).clf()
         figom = figure(30)
         axom = figom.gca()
@@ -635,7 +451,7 @@ def setupfigs(finf,fn):
         figom.colorbar(hiom,ax=axom)
 
     hpmn = None; hpmd = None; medpl = None; meanpl = None
-    if showmeanmedian:
+    if 'meanmedian' in pshow:
         medpl = np.zeros(finf['frameind'].size, dtype=float) #don't use nan, it won't plot
         meanpl = medpl.copy()
         figure(31).clf()
@@ -652,7 +468,7 @@ def setupfigs(finf,fn):
 
     detect = np.nan #in case it falls through to h5py writer
     hpdt = None; fgdt = None
-    if plotdet or savedet:
+    if 'det' in pshow or savedet:
         detect = np.zeros(finf['frameind'].size, dtype=int)
         figure(40).clf()
         fgdt = figure(40)
@@ -665,42 +481,6 @@ def setupfigs(finf,fn):
 
     return {'iofm':hiom, 'pmean':hpmn, 'pmed':hpmd, 'median':medpl, 'mean':meanpl,
             'pdet':hpdt, 'fdet':fgdt, 'detect':detect}
-
-def draw_flow(img, flow, step=16):
-    """
-    this came from opencv/examples directory
-    another way: http://docs.opencv.org/trunk/doc/py_tutorials/py_gui/py_drawing_functions/py_drawing_functions.html
-    """
-    #scaleFact = 1. #arbitary factor to make flow visible
-    canno = (0, 65535, 0)  # 65535 since it's 16-bit images
-    h, w = img.shape[:2]
-    y, x = np.mgrid[step//2:h:step, step//2:w:step].reshape(2,-1)
-    fx, fy =  flow[y,x].T
-    #create line endpoints
-    lines = np.vstack([x, y, (x+fx), (y+fy)]).T.reshape(-1, 2, 2)
-    lines = np.int32(lines + 0.5)
-    #create image and draw
-    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-
-    cv2.polylines(vis, lines, isClosed=False, color=canno, thickness=1, lineType=8)
-    #set_trace()
-    #draw filled green circles
-    for (x1, y1), (x2, y2) in lines:
-        cv2.circle(vis, center=(x1, y1), radius=1, color=canno, thickness=-1)
-    return vis
-
-def draw_hsv(flow):
-    scaleFact = 10 #arbitary factor to make flow visible
-    h, w = flow.shape[:2]
-    fx, fy = scaleFact*flow[:,:,0], scaleFact*flow[:,:,1]
-    ang = np.arctan2(fy, fx) + np.pi
-    mag = np.hypot(fx,fy)
-    hsv = np.empty((h, w, 3), np.uint8)
-    hsv[...,0] = ang*(180/np.pi/2)
-    hsv[...,1] = 255
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    return rgb
 
 def getvidinfo(fn,cp,up,verbose):
     print('using {} for {}'.format(cp['ofmethod'],fn))
