51434c8024ef3c3d76123ab028c8deb29087a598
scivision <scivision@users.noreply.github.com>
2016-01-17 14:38:25 -0500

    code reuse

--- RunDetectAurora.py
+++ RunDetectAurora.py
@@ -5,6 +5,7 @@ Michael Hirsch
 """
 from __future__ import absolute_import
 from pathlib2 import Path
+#
 from cvhst.detectaurora import loopaurorafiles
 
 def rundetect(p):
@@ -29,7 +30,7 @@ def rundetect(p):
     try:
         #note, if a specific file is given, vidext is ignored
         flist = sorted(Path(p.indir).expanduser().glob('*'+p.vidext))
-        print('working with {} files.'.format(len(flist)))
+        print('found {} {} files in {}'.format(len(flist),p.vidext,p.indir))
 
         if p.profile:
             import cProfile,pstats
@@ -39,7 +40,7 @@ def rundetect(p):
         else:
             loopaurorafiles(flist, uparams, savevideo, p.framebyframe, p.verbose)
     except KeyboardInterrupt:
-        exit('aborting per user request')
+        print('aborting per user request')
 
 if __name__=='__main__':
     from argparse import ArgumentParser
@@ -47,7 +48,7 @@ if __name__=='__main__':
     p.add_argument('indir',help='specify file, OR top directory over which to recursively find video files')
     p.add_argument('-e','--vidext',help='suffix of raw video file (.DMCdata,.h5,.fits)',default='.h5')
     p.add_argument('--fps',help='output file FPS (note VLC needs fps>=3)',type=float,default=3)
-    p.add_argument('-p','--framebyframe',help='space bar toggles play/pause', action='store_true')
+    p.add_argument('-b','--framebyframe',help='space bar toggles play/pause', action='store_true')
     p.add_argument('-s','--savevideo',help='save video at each step (can make enormous files)',action='store_true')
     p.add_argument('-t','--savetiff',help='save tiff at each step (can make enormous files)',action='store_true')
     p.add_argument('-k','--step',help='frame step skip increment (default 10000)',type=int,default=1)
@@ -57,7 +58,7 @@ if __name__=='__main__':
     p.add_argument('-c','--contrast',help='[low high] data numbers to bound video contrast',type=int,nargs=2,default=(None,)*2)
     p.add_argument('--rejectvid',help='reject raw video files with less than this many frames',type=int,default=10)
     p.add_argument('-r','--rejectdet',help='reject files that have fewer than this many detections',type=int,default=10)
-    p.add_argument('--paramfn',help='parameter file for cameras',default='camparam.xlsx')
+    p.add_argument('-p','--paramfn',help='parameter file for cameras',default='camparam.xlsx')
     p.add_argument('-v','--verbose',help='verbosity',action='store_true')
     p.add_argument('--profile',help='profile debug',action='store_true')
     p = p.parse_args()
