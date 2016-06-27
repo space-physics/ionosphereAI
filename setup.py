7f2e483a1e4ecfe65b88791d6e88a4a86efec259
scivision <scivision@users.noreply.github.com>
2016-06-26 23:14:11 -0400

    doc

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from setuptools import setup
 import subprocess
-
 
 try:
     subprocess.call(['conda','install','--yes','--file','requirements.txt'])
 except Exception as e:
     pass
 
-
-with open('README.rst','r') as f:
-	long_description = f.read()
-
-
 setup(name='cvhst',
+      packages=['cvhst'],
   	  description='OpenCV auroral detection for the HiST auroral tomography system',
-	   long_description=long_description,
-	   author='Michael Hirsch',
 	   url='https://github.com/scienceopen/cvhst',
-    dependency_links = ['https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
-                          'https://github.com/scienceopen/cvutils/tarball/master#egg=cvutils'],
+       dependency_links = [
+            'https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
+            'https://github.com/scienceopen/cvutils/tarball/master#egg=cvutils'],
 	   install_requires=['histutils','cvutils',
                         'tifffile',
                         ],
---
+++ tests/test.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+from cvhst.detectaurora import loopaurorafiles
