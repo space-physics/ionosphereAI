ff39177ab59d9972832f24161dafee242b873551
Michael H <nobody@github.com>
2016-08-07 02:18:25 -0400

    init

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.call(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--file','requirements.txt'])
 except Exception as e:
     pass
 
-setup(name='cvhst',
-      packages=['cvhst'],
-  	  description='OpenCV auroral detection for the HiST auroral tomography system',
-	   url='https://github.com/scienceopen/cvhst',
+setup(name='cviono',
+      packages=['cviono'],
+  	  description='OpenCV auroral detection and passive FM radar ionosphere turbulence detector',
+	   url='https://github.com/scienceopen/cv_ionosphere',
        dependency_links = [
             'https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
             'https://github.com/scienceopen/cvutils/tarball/master#egg=cvutils'],
